737a8d43987ebdc30f7a62d62eafc728e1e8ddca
falt2020 <kadenko05@gmail.com>
2025-04-10 14:56:07 +0200

    Add Students Modal

--- src/components/Students.tsx
+++ src/components/Students.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
-import Image from 'next/image'
+'use client'
+
+import React, { useState } from 'react';
+import Image from 'next/image';
+import ModalStudent from './ModalStudent';
 
 const Students = () => {
+
+    const [isModalOpen, setModalOpen] = useState(false);
+
+    const openModal = () => setModalOpen(true);
+    const closeModal = () => setModalOpen(false);
+
     return (
         <section>
-            <div className=''>
+            <ModalStudent inOpen={isModalOpen} onClose={closeModal}/>
+            <div>
                 <button 
+                    onClick={openModal}
                     className='flex bg-[#4CBC9A] w-[207px] h-[60px] mt-[56px] justify-center items-center rounded-[40px] text-white cursor-pointer ml-auto mr-[50px]'
                 >
-                    <Image src="/plus.png" alt="" width={16} height={16} className='mr-[16px] cursor-pointer'/>Add Students
+                    <Image src="/plus.png" alt="Add Student" width={16} height={16} className='mr-[16px] cursor-pointer'/>
+                    Add Students
                 </button>
             </div>
         </section>
-
-        
     );
 };
 
